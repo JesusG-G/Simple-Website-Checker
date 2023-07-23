49a2c12b5ca0fd812e0652d558f2514d933eacbe
JesusG-G <jesusguillermogonzalezguaita@gmail.com>
2023-07-23 19:09:50 -0400

    [JESUS] -  function check_website updated

--- src/website_checker.py
+++ src/website_checker.py
@@ -34,18 +34,11 @@ class WebsiteChecker():
             
         return '(???) Unknown status code...'
 
-    def check_website(self,website:str, user_agent):
+    def check_website(self,website:str, user_agent) -> str:
         try:
             code:int = requests.get(website, headers={'User-Agent': user_agent}).status_code
-            print(website, self.get_status_description(code))
+            result = f"{website} {self.get_status_description(code)}"
+            return result
         except Exception:
-            print(f'**Could not get information for website: {website}')
-
-#def main():
-#    sites: list[str] = get_websites('src/websites.csv')
-#    user_agent: str = get_user_agent()
-#    for site in sites:
-#        check_website(site,user_agent)
-
-#if __name__ == "__main__":
-#    main()
+            result = f'**Could not get information for website: {website}'
+            return result
